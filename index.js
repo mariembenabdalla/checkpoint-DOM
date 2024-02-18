621a4e3ac2fa0f71e1b2927f224dff9c704f401f
mariembenabdalla <mariembenabdallah020@gmail.com>
2024-02-18 14:00:09 +0100

    checkpoint DOM

---
+++ index.js
@@ -0,0 +1,49 @@
+var btn_plus = document.querySelectorAll(".plus");
+var btn_min = document.querySelectorAll(".min");
+var price = document.querySelectorAll(".price");
+var totalprice = document.getElementById("Totale Price");
+var quantity = document.querySelectorAll(".quantity");
+var btn_delete = document.querySelectorAll(".delete");
+var icon = document.querySelectorAll(".fa-heart");
+
+console.log(quantity);
+
+for (let btn of btn_plus) {
+  btn.addEventListener("click", function () {
+    let parent = btn.parentElement;
+    let quantity = parent.querySelector(".quantity");
+    quantity.innerHTML++;
+    totalprice.innerHTML =
+      +totalprice.innerHTML + +parent.querySelector(".price").innerHTML;
+  });
+}
+for (let btn of btn_min) {
+  btn.addEventListener("click", function () {
+    let parent = btn.parentElement;
+    let quantity = parent.querySelector(".quantity");
+    if (parseInt(quantity.innerHTML) > 0) {
+      quantity.innerHTML--;
+      totalprice.innerHTML =
+        +totalprice.innerHTML - parent.querySelector(".price").innerHTML;
+    }
+  });
+}
+for (let btn of btn_delete) {
+  btn.addEventListener("click", function () {
+    let parent = btn.parentElement;
+    let quantity = parent.querySelector(".quantity");
+    let price = parent.querySelector(".price");
+    let total = parseInt(totalprice.innerHTML);
+    console.log(total);
+    if (total > 0) {
+      totalprice.innerHTML =
+        total - parseInt(quantity.innerHTML) * parseInt(price.innerHTML);
+    }
+    quantity.innerHTML = 0;
+  });
+}
+icon.forEach(function (icon) {
+  icon.addEventListener("click", function () {
+    icon.classList.toggle("active");
+  });
+});
